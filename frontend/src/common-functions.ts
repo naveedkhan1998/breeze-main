73a5e14ac9eda051f83c7938a63058ffaf956f73
nkhan364 <naveedkhan13041998@gmail.com>
2024-09-24 06:28:27 -0400

    formatting

--- frontend/src/common-functions.ts
+++ frontend/src/common-functions.ts
@@ -7,17 +7,6 @@ export function formatDate(originalDate: string) {
   return parsedIndianDate;
 }
 
-// tickMarkFormatter: (time: number) => {
-//   const date = new Date(time * 1000);
-//   const hours = date.getHours();
-//   const minutes = date.getMinutes();
-//   const period = hours >= 12 ? "PM" : "AM";
-//   const formattedHours = hours % 12 || 12;
-//   return `${formattedHours}:${
-//     minutes < 10 ? "0" : ""
-//   }${minutes} ${period}`;
-// },
-
 import { CandlestickData, LineData } from "lightweight-charts";
 import { Candle } from "./common-types";
 
