648240d0e5a768e01bfd3868478bbdd066f37ac8
nkhan364 <naveedkhan13041998@gmail.com>
2025-07-07 17:32:45 -0400

    fix: comment out collectstatic command and update loglevel for Celery scripts

--- backend/scripts/production/run_celery_worker_prod.sh
+++ backend/scripts/production/run_celery_worker_prod.sh
@@ -7,5 +7,5 @@ echo "STARTING HTTP SERVER..."
 python3 http_server.py & # Run HTTP server in background
 
 echo "STARTING CELERY WORKER WITH MEMORY LIMIT..."
-celery -A main worker --loglevel=DEBUG --time-limit=0 --concurrency=1
+celery -A main worker --loglevel=ERROR --time-limit=0 --concurrency=1
 #celery multi start w1 w2 -A main --loglevel=INFO
