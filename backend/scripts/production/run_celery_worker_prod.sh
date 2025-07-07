35ee0b067c91afaedafa07b732732aa6b5c674bc
nkhan364 <naveedkhan13041998@gmail.com>
2025-07-07 16:50:56 -0400

    feat: add production scripts for Celery Beat, Worker, and Flower

---
+++ backend/scripts/production/run_celery_worker_prod.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set the maximum memory limit to 500 MB
+ulimit -v $((512 * 1024))
+
+echo "STARTING HTTP SERVER..."
+python3 http_server.py & # Run HTTP server in background
+
+echo "STARTING CELERY WORKER WITH MEMORY LIMIT..."
+celery -A main worker --loglevel=DEBUG --time-limit=0 --concurrency=1
+#celery multi start w1 w2 -A main --loglevel=INFO
