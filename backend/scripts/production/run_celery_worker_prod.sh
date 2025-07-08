cc0dbde23a93e9914d4030ac4a01878484ec1c7e
nkhan364 <naveedkhan13041998@gmail.com>
2025-07-07 21:25:54 -0400

    refactor: clean up Celery settings and improve memory limit configuration in production scripts

--- backend/scripts/production/run_celery_worker_prod.sh
+++ backend/scripts/production/run_celery_worker_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set the maximum memory limit to 500 MB
-#ulimit -v $((512 * 1024))
+ulimit -v $((512 * 1024))
 
 echo "STARTING HTTP SERVER..."
 python3 http_server.py & # Run HTTP server in background
@@ -10,6 +10,5 @@ echo "STARTING CELERY WORKER WITH MEMORY LIMIT..."
 celery -A main worker \
     --loglevel=ERROR \
     --time-limit=0 \
-    --concurrency=1 \
-    --pool=solo
+    --concurrency=1
 #celery multi start w1 w2 -A main --loglevel=INFO
