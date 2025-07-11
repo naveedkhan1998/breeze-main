cac8d04772a815c847bde44bf22df1cffc61e879
nkhan364 <naveedkhan13041998@gmail.com>
2025-07-11 18:23:21 -0400

    feat: add backend migrations worker configuration; streamline database migration process in deployment

--- backend/scripts/production/run_backend_prod.sh
+++ backend/scripts/production/run_backend_prod.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-python3 manage.py wait_for_db &
-
-wait
-#python3 manage.py collectstatic --noinput --skip-checks
-python3 manage.py migrate --noinput --skip-checks
-python3 manage.py initadmin
-
 echo "STARTING GUNICORN SERVER..."
 gunicorn main.wsgi:application -t 1800 --bind :8000 --log-level=info
